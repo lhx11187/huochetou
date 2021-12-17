1a6c686ccb1c1fef2128e453fb3b511938a6020d
Blackduck <lhx11187@qq.com>
2021-12-17 15:48:36 +0800

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -9,7 +9,7 @@ mkdir /root/filebrowser &
 wget -c -O /root/linux-amd64-filebrowser.tar.gz https://github.com/filebrowser/filebrowser/releases/download/v2.19.0/linux-amd64-filebrowser.tar.gz
 cd /root/filebrowser && tar -zxvf /root/linux-amd64-filebrowser.tar.gz
 chmod 755 /root/filebrowser/filebrowser
-cd /root/filebrowser && /root/filebrowser/filebrowser -p 8081 &
+#cd /root/filebrowser && /root/filebrowser/filebrowser -p 8081 &
 
 wget -c -O /root/frp_0.38.0_linux_amd64.tar.gz https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
 cd /root && tar -zxvf /root/frp_0.38.0_linux_amd64.tar.gz
