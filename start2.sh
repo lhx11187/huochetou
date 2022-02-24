253d0513b4aabe2b81b99ece5e63fa3bb890db52
Blackduck <lhx11187@qq.com>
2022-02-24 10:25:48 +0800

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -32,7 +32,7 @@ chmod 755 /root/webdav/start.sh
 #killall -9 qbittorrent-nox
 #killall -9 verysync 
 #killall -9 filebrowser 
-#service nginx restart 
+#service nginx restart
 
 killall -9 v2ray
 cd /root/v2ray && ./start.sh >/dev/null 2>&1 &
@@ -41,7 +41,7 @@ mkdir /root/filebrowser &
 wget -c -O /root/linux-amd64-filebrowser.tar.gz https://github.com/filebrowser/filebrowser/releases/download/v2.19.0/linux-amd64-filebrowser.tar.gz
 cd /root/filebrowser && tar -zxvf /root/linux-amd64-filebrowser.tar.gz
 chmod 755 /root/filebrowser/filebrowser
-#cd /root/filebrowser && /root/filebrowser/filebrowser -p 8081 &
+cd /root/filebrowser && /root/filebrowser/filebrowser -p 8081 &
 
 wget -c -O /root/frp_0.38.0_linux_amd64.tar.gz https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
 cd /root && tar -zxvf /root/frp_0.38.0_linux_amd64.tar.gz
@@ -65,4 +65,4 @@ unzip /root/AriaNg-1.2.3.zip -d /var/www/html/AriaNg
 
 rclone rcd --rc-web-gui --rc-user lhx11187 --rc-pass 7702843 >/dev/null 2>&1 &
 
-
+service nginx restart
